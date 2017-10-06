9cb9ba6eaa9e9502f3768cd8d2c338a05a65324a
Vitaliy Stoliarov <huccah@gmail.com>
2017-10-06 23:00:14 +0300

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'build/texturity.js',
+        format: 'iife'
+    },
+    sourcemap: true,
+    plugins: [],
+    name: 'Texturity'
+};
