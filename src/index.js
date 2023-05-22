497fc9fb30309a6dca90d200e8e375c85237d9a6
Ni55aN <huccah@gmail.com>
2023-05-22 11:20:16 +0300

    toCanvas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ShaderManager from './shader-manager';
-import {FloatFrameBuffer} from './framebuffer';
+import { FloatFrameBuffer } from './framebuffer';
 import BrickGenerator from './generators/brick';
 import TransformGenerator from './generators/transform';
 
@@ -17,12 +17,23 @@ export class Canvas {
         this.w = w;
         this.h = h;
         this.backup = null;
-        
+
         gl = initGL();
         gl.clearColor(...clearColor);
-            
+
         resize(w, h);
         gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+
+    toCanvas(w, h) {
+        const destinationCanvas = document.createElement('canvas')
+        destinationCanvas.width = w
+        destinationCanvas.height = h
+        const ctx = destinationCanvas.getContext('2d');
+
+        ctx.drawImage(element, 0, 0, w, h);
+
+        return destinationCanvas
     }
 
     save() {
@@ -47,7 +58,7 @@ export class Canvas {
 
         return this;
     }
-	
+
     drawBuffer(vertices) {
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
         gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
@@ -72,14 +83,14 @@ export class Canvas {
             x, y]);
         return this;
     }
-	
+
     drawRadialGradient(color1, color2) {
         useProgram(programs.radialGradient);
         gl.uniform3fv(gl.getUniformLocation(programs.radialGradient, 'color1'), color1);
         gl.uniform3fv(gl.getUniformLocation(programs.radialGradient, 'color2'), color2);
         this.drawRect(0, 0, this.w, this.h);
         useProgram(programs.simple);
-        
+
         return this;
     }
 
@@ -92,17 +103,17 @@ export class Canvas {
         var bricks = generator.getBricks(this.w, this.h);
 
         bricks.forEach(r => this.drawRect(...r));
-        
+
         return this;
     }
 
     drawCircle(r) {
         useProgram(programs.circle);
         gl.uniform1f(gl.getUniformLocation(programs.circle, 'r'), r);
-        
+
         this.drawRect(0, 0, this.w, this.h);
         useProgram(programs.simple);
-        
+
         return this;
     }
 
@@ -121,7 +132,7 @@ export class Canvas {
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW);
         gl.enableVertexAttribArray(1);
         gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
-		
+
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.enableVertexAttribArray(0);
         gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
@@ -129,24 +140,24 @@ export class Canvas {
         gl.bindTexture(gl.TEXTURE_2D, texture);
         params.forEach(param => {
             gl.texParameteri(gl.TEXTURE_2D, param[0], param[1]);
-        });    
+        });
 
         if (x instanceof Array)
             x.forEach(r => this.drawRect(...r));
         else
-        	this.drawRect(x, y, w, h);
+            this.drawRect(x, y, w, h);
         gl.disableVertexAttribArray(1);
 
         useProgram(programs.simple);
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
         gl.deleteBuffer(uvBuffer);
-  
+
         return this;
     }
 
     cropTexture(texture, [origw, origh], x, y, w, h) {
         resize(w, h);
-    
+
         this.drawTexture(texture, -x, -y, origw, origh);
     }
 
@@ -168,7 +179,7 @@ export class Canvas {
 
         return this;
     }
-	
+
     blur(texture, iterations) {
         iterations = Math.min(iterations, 100);
         var kernel = new Array(9).fill(1 / 9);
@@ -195,9 +206,9 @@ export class Canvas {
         gl.uniform1i(gl.getUniformLocation(programs.convolution, 'texture'), texture);
         gl.uniform1fv(gl.getUniformLocation(programs.convolution, 'matrix'),
             new Float32Array(matrix));
-        
+
         this.drawBuffer([-1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1]);
-        
+
         useProgram(programs.simple);
         return this;
     }
@@ -206,7 +217,7 @@ export class Canvas {
         var blendProgram = shaderManager.createShaderRuntime('blend', b, expression, preExpressions);
 
         useProgram(blendProgram);
-        
+
         if (b instanceof WebGLTexture) {
             var tex2 = b;
 
@@ -229,13 +240,13 @@ export class Canvas {
 
         useProgram(programs.simple);
         gl.activeTexture(gl.TEXTURE0);
-        
+
         return this;
     }
 
     neighbors(texture, expression) {
         var neighborsProgram = shaderManager.createShaderRuntime('neighbors', expression);
-        
+
         useProgram(neighborsProgram);
 
         gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -247,18 +258,18 @@ export class Canvas {
     }
 
     drawFourierSpectrum(texture) {
-       
+
         this.blend(texture, element.width * element.height, 'vec3(a.z/sqrt(b+a.z*a.z))');
-        var uv = 
-        [
-            -0.5, 0.5,
-            0.5, 0.5,
-            0.5, -0.5,
-            
-            0.5, -0.5,
-            -0.5, -0.5,
-            -0.5, 0.5
-        ]
+        var uv =
+            [
+                -0.5, 0.5,
+                0.5, 0.5,
+                0.5, -0.5,
+
+                0.5, -0.5,
+                -0.5, -0.5,
+                -0.5, 0.5
+            ]
         var params = [[gl.TEXTURE_WRAP_T, gl.REPEAT], [gl.TEXTURE_WRAP_S, gl.REPEAT]];
 
         texture = this.toTexture();
@@ -270,18 +281,18 @@ export class Canvas {
 
     fourierFilter(texture, mask) {
         var fb = new FloatFrameBuffer(gl, element.width, element.height);
-        
+
         if (mask instanceof WebGLTexture) { /// shift mask
             var uv =
-                    [
-                        -0.5, 0.5,
-                        0.5, 0.5,
-                        0.5, -0.5,
-            
-                        0.5, -0.5,
-                        -0.5, -0.5,
-                        -0.5, 0.5
-                    ]
+                [
+                    -0.5, 0.5,
+                    0.5, 0.5,
+                    0.5, -0.5,
+
+                    0.5, -0.5,
+                    -0.5, -0.5,
+                    -0.5, 0.5
+                ]
             var params = [[gl.TEXTURE_WRAP_T, gl.REPEAT], [gl.TEXTURE_WRAP_S, gl.REPEAT]];
 
             this.drawTexture(mask, 0, 0, element.width, element.height, params, uv)
@@ -316,7 +327,7 @@ export class Canvas {
         this.drawBuffer([-1, -1, -1, 1, 1, 1, 1, 1, 1, -1, -1, -1]);
 
         useProgram(programs.simple);
-        
+
         return this;
     }
 
@@ -341,9 +352,9 @@ export class Canvas {
             element.toBlob((blob) => {
                 resolve(URL.createObjectURL(blob))
             }, 'image/png');
-        });    
+        });
     }
-    
+
     async toImage() {
         var canv = this;
 
@@ -368,7 +379,7 @@ export class Canvas {
 export function disposeTexture(texture) {
     var index = textures.indexOf(texture);
 
-    if (index !==-1)
+    if (index !== -1)
         textures.splice(index, 1);
     gl.deleteTexture(texture);
 }
@@ -387,7 +398,7 @@ export function createShader(vertex, fragment) {
 
 export function initGL(contextName = 'webgl', params = {}) {
     if (gl) return gl;
-  
+
     params = Object.assign({
         alpha: false,
         antialias: false,
@@ -396,7 +407,7 @@ export function initGL(contextName = 'webgl', params = {}) {
 
     element = document.createElement('canvas');
     gl = element.getContext(contextName, params);
-    format = params.alpha ? gl.RGBA : gl.RGB; 
+    format = params.alpha ? gl.RGBA : gl.RGB;
 
     shaderManager = new ShaderManager(gl);
     programs = shaderManager.createPrecompiledShaders();
@@ -407,32 +418,32 @@ export function initGL(contextName = 'webgl', params = {}) {
 
 export function createBuffer() {
     var buffer = gl.createBuffer();
-    
+
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    
+
     var posAttr = gl.getAttribLocation(programs.simple, 'position');
-    
+
     gl.enableVertexAttribArray(posAttr);
     gl.vertexAttribPointer(posAttr, 2, gl.FLOAT, false, 0, 0);
-    
+
     return buffer;
 }
 
 export function resize(w, h) {
     element.width = w;
     element.height = h;
-    
+
     gl.viewport(0, 0, w, h);
     useProgram(programs.simple);
 }
 
 export function loadTexture(img) {
     if (!(img instanceof Image)) throw 'argument should be instance of Image';
-    
+
     var tex = gl.createTexture();
 
     gl.bindTexture(gl.TEXTURE_2D, tex);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); 
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
